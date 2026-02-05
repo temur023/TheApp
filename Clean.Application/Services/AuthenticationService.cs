733dc37a847c8401de4ec11bf51e66536b0e7fa0
Temurmalik <temurmalikirgashev@gmail.com>
2026-02-05 13:49:40 +0500

    added dockerfile

--- Clean.Application/Services/AuthenticationService.cs
+++ Clean.Application/Services/AuthenticationService.cs
@@ -6,6 +6,7 @@ using Clean.Application.Dtos;
 using Clean.Application.Responses;
 using Clean.Domain.Entities;
 using FluentEmail.Core;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
@@ -17,56 +18,46 @@ public class AuthenticationService(IAuthenticationRepository repository,IConfigu
 
     public async Task<Response<string>> Register(UserCreateDto user)
     {
+        if (string.IsNullOrWhiteSpace(user.Password))
+            return new Response<string>(400, "Password is required.");
+
+        if (user.Password != user.CheckPassword)
+            return new Response<string>(400, "Passwords do not match.");
+
+        var model = new User
+        {
+            FullName = user.FullName,
+            Email = user.Email,
+            Password = user.Password,
+            DateOfRegistration = DateTimeOffset.UtcNow,
+            LastSeen = DateTimeOffset.UtcNow,
+            Status = Status.Unverified
+        };
+
+        var token = new EmailVerificationToken
+        {
+            Id = Guid.NewGuid(),
+            UserId = model.Id,
+            Created = DateTimeOffset.UtcNow,
+            Expires = DateTimeOffset.UtcNow.AddDays(1)
+        };
+
         try
         {
-            var model = new User
-            {
-                FullName = user.FullName,
-                Email = user.Email,
-                Password = user.Password,
-                DateOfRegistration = DateTimeOffset.UtcNow,
-                LastSeen = DateTimeOffset.UtcNow,
-                Status = Status.Unverified
-            };
-            if (model.Password.Length < 1)
-                return new Response<string>(400, "Password should contain al least a charecter");
-            if (model.Password != user.CheckPassword)
-                return new Response<string>(400, "Passwords do not match!");
-            DateTimeOffset utcNow = DateTimeOffset.UtcNow;
-            var verificationToken = new EmailVerificationToken()
-            {
-                Id = Guid.NewGuid(),
-                UserId = model.Id,
-                Created = utcNow,
-                Expires = utcNow.AddDays(1)
-            };
-            await repository.Register(model,verificationToken);
-            string verificationLink = linkFactory.Create(verificationToken);
-
-            _ = Task.Run(async () => 
-            {
-                try
-                {
-                    await fluentEmail
-                        .To(user.Email)
-                        .Subject("Confirmation for login!")
-                        .Body($"<a href='{verificationLink}'>Click here</a> to confirm!", true)
-                        .SendAsync();
-                    return new Response<string>(200, "The email sent");
-                }
-                catch (Exception ex)
-                {
-                    return new Response<string>(500, "Failed to send verification email.");
-                }
-                
-            });
-
-            return new Response<string>(200, "The user has been registered!");
-        }catch(Microsoft.EntityFrameworkCore.DbUpdateException)
-        {
-            return new Response<string>(400, "User with this email already exists!");
+            await repository.Register(model, token);
         }
-       
+        catch (DbUpdateException)
+        {
+            return new Response<string>(400, "User with this email already exists.");
+        }
+        
+        _ = fluentEmail
+            .To(user.Email)
+            .Subject("Confirmation for login!")
+            .Body("Click the link to confirm", true)
+            .SendAsync();
+
+        return new Response<string>(200, "User registered successfully.");
     }
 
     public async Task<Response<string>> Login(UserLoginDto user)
